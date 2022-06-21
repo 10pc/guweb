5cea4ce8ee4055bc636417424635ac146bbaade9
kaupec <7bsanur@gmail.com>
2022-06-22 04:50:57 +0700

    I still don't know what I'm doing

--- main.py
+++ main.py
@@ -28,35 +28,38 @@ app.secret_key = glob.config.secret_key
 @app.before_serving
 async def mysql_conn() -> None:
     glob.db = AsyncSQLPool()
-    await glob.db.connect(glob.config.mysql) # type: ignore
+    await glob.db.connect(glob.config.mysql)
     log('Connected to MySQL!', Ansi.LGREEN)
 
 @app.before_serving
 async def http_conn() -> None:
-    glob.http = aiohttp.ClientSession(json_serialize=lambda x: orjson.dumps(x).decode())
+    glob.http = aiohttp.ClientSession(json_serialize=orjson.dumps)
     log('Got our Client Session!', Ansi.LGREEN)
 
-@app.after_serving
-async def shutdown() -> None:
-    await glob.db.close()
-    await glob.http.close()
-
 # globals which can be used in template code
+_version = repr(version)
+@app.before_serving
 @app.template_global()
 def appVersion() -> str:
-    return repr(version)
+    return _version
 
+_app_name = glob.config.app_name
+@app.before_serving
 @app.template_global()
 def appName() -> str:
-    return glob.config.app_name
+    return _app_name
 
+_captcha_key = glob.config.hCaptcha_sitekey
+@app.before_serving
 @app.template_global()
 def captchaKey() -> str:
-    return glob.config.hCaptcha_sitekey
+    return _captcha_key
 
+_domain = glob.config.domain
+@app.before_serving
 @app.template_global()
 def domain() -> str:
-    return glob.config.domain
+    return _domain
 
 from blueprints.frontend import frontend
 app.register_blueprint(frontend)
@@ -64,11 +67,14 @@ app.register_blueprint(frontend)
 from blueprints.admin import admin
 app.register_blueprint(admin, url_prefix='/admin')
 
+from blueprints.gw_api import gw_api
+app.register_blueprint(gw_api, url_prefix='/gw_api')
+
 @app.errorhandler(404)
 async def page_not_found(e):
     # NOTE: we set the 404 status explicitly
     return (await render_template('404.html'), 404)
 
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 if __name__ == '__main__':
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    app.run(port=8000, debug=glob.config.debug) # blocking call
+    app.run(debug=glob.config.debug) # blocking call
